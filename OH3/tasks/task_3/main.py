237bfe3511f31d4ff80eeb3e1effd77f26b828bb
Ktsarvi <alasgarovtimur@gmail.com>
2026-09-02 23:51:00 +0400

    OH3 finish

--- OH3/tasks/task_3/main.py
+++ OH3/tasks/task_3/main.py
@@ -1,11 +1,9 @@
-from pathlib import Path
+
 import text_utils
 
-file_path = Path(__file__).parent / "sample_speech.txt"
-
-words = text_utils.count_words(file_path)
-lines = text_utils.count_lines(file_path)
-top5 = text_utils.most_common_words(file_path, 5)
+words = text_utils.count_words("sample_speech.txt")
+lines = text_utils.count_lines("sample_speech.txt")
+top5 = text_utils.most_common_words("sample_speech.txt", 5)
 
 print(f"Words: {words}")
 print(f"Lines: {lines}")
